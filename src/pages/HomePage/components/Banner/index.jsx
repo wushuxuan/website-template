6652fae4fa8568aaa374932f26a32b07fa212a8e
吴姝璇 <wshuxuan@extremevalue.cn>
2019-12-27 14:21:15 +0800

    更新

--- src/pages/HomePage/components/Banner/index.jsx
+++ src/pages/HomePage/components/Banner/index.jsx
@@ -5,8 +5,8 @@ export default function Banner() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.title}>ICE DESIGN 海量物料</div>
-        <div className={styles.desc}>淘宝中后台 & 让前端开发简单而友好</div>
+        <div className={styles.title}>官网模板</div>
+        <div className={styles.desc}>简约便捷</div>
         <a className={styles.link}>开始使用</a>
       </div>
     </div>
